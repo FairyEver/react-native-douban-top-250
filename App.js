e47b2d70c36d6d260c7ebfa4f8eec853e538cfe8
liyang <1711467488@qq.com>
2018-05-03 23:08:49 +0800

    no message

--- App.js
+++ App.js
@@ -19,6 +19,7 @@ export default class App extends React.Component {
         <TabBarIOS.Item
           title="TOP250"
           icon={require('./src/image/icon/tabbar/fire-normal.png')}
+          selectedIcon={require('./src/image/icon/tabbar/fire-active.png')}
           selected={this.state.activeTab === 'top'}
           onPress={() => {this.setState({activeTab: 'top'})}}
         >
@@ -27,6 +28,7 @@ export default class App extends React.Component {
         <TabBarIOS.Item
           title="新片榜"
           icon={require('./src/image/icon/tabbar/new-normal.png')}
+          selectedIcon={require('./src/image/icon/tabbar/new-active.png')}
           selected={this.state.activeTab === 'new'}
           onPress={() => {this.setState({activeTab: 'new'})}}
         >
@@ -35,6 +37,7 @@ export default class App extends React.Component {
         <TabBarIOS.Item
           title="正在热映"
           icon={require('./src/image/icon/tabbar/now-normal.png')}
+          selectedIcon={require('./src/image/icon/tabbar/now-active.png')}
           selected={this.state.activeTab === 'now'}
           onPress={() => {this.setState({activeTab: 'now'})}}
         >
@@ -43,6 +46,7 @@ export default class App extends React.Component {
         <TabBarIOS.Item
           title="即将上映"
           icon={require('./src/image/icon/tabbar/coming-normal.png')}
+          selectedIcon={require('./src/image/icon/tabbar/coming-active.png')}
           selected={this.state.activeTab === 'coming'}
           onPress={() => {this.setState({activeTab: 'coming'})}}
         >
@@ -51,6 +55,7 @@ export default class App extends React.Component {
         <TabBarIOS.Item
           title="搜索"
           icon={require('./src/image/icon/tabbar/search-normal.png')}
+          selectedIcon={require('./src/image/icon/tabbar/search-active.png')}
           selected={this.state.activeTab === 'search'}
           onPress={() => {this.setState({activeTab: 'search'})}}
         >
