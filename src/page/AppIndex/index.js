68988bf70ebefcef7188b5848ca9efd13c201bb4
liyang <1711467488@qq.com>
2018-05-06 22:35:41 +0800

    no message

--- src/page/AppIndex/index.js
+++ src/page/AppIndex/index.js
@@ -1,31 +1,26 @@
 import React from 'react';
 import { View, Text, TabBarIOS } from 'react-native';
 
+import unit from '../../unit/index.js';
+
 import PageTop250 from '../PageTop250';
 
-export default class PageTabBar extends React.Component {
-
-  // 颜色设置
-  COLOR_YELLOW = '#fbb03d';
-  COLOR_GREEN = '#23a06e';
-  COLOR_BLUE = '#2490f8';
-  COLOR_PURPLE = '#7f25fb';
-  COLOR_TOP250 = '#fc1368';
+export default class AppIndex extends React.Component {
 
   state = {
     activeTab: 'top',
-    tintColor: this.COLOR_TOP250
+    tintColor: unit.COLOR.TOP250
   };
 
   // 切换tab
   toggleTab = (tabName) => {
     let color = ''
     switch (tabName) {
-      case 'top': color = this.COLOR_TOP250; break;
-      case 'new': color = this.COLOR_PURPLE; break;
-      case 'now': color = this.COLOR_YELLOW; break;
-      case 'coming': color = this.COLOR_BLUE; break;
-      default: color = this.COLOR_GREEN; break;
+      case 'top': color = unit.COLOR.TOP250; break;
+      case 'new': color = unit.COLOR.PURPLE; break;
+      case 'now': color = unit.COLOR.YELLOW; break;
+      case 'coming': color = unit.COLOR.BLUE; break;
+      default: color = unit.COLOR.GREEN; break;
     };
     this.setState({
       activeTab: tabName,
@@ -36,16 +31,13 @@ export default class PageTabBar extends React.Component {
   render () {
     return (
       <View style={{flex: 1}}>
-        <TabBarIOS
-          tintColor={this.state.tintColor}
-        >
+        <TabBarIOS tintColor={this.state.tintColor}>
           <TabBarIOS.Item
             title="TOP250"
             icon={require('../../image/icon/tabbar/fire-normal.png')}
             selectedIcon={require('../../image/icon/tabbar/fire-active.png')}
             selected={this.state.activeTab === 'top'}
-            onPress={() => {this.toggleTab('top')}}
-          >
+            onPress={() => {this.toggleTab('top')}}>
             <PageTop250 navigator={this.props.navigator}/>
           </TabBarIOS.Item>
           <TabBarIOS.Item
@@ -53,8 +45,7 @@ export default class PageTabBar extends React.Component {
             icon={require('../../image/icon/tabbar/new-normal.png')}
             selectedIcon={require('../../image/icon/tabbar/new-active.png')}
             selected={this.state.activeTab === 'new'}
-            onPress={() => {this.toggleTab('new')}}
-          >
+            onPress={() => {this.toggleTab('new')}}>
             <Text>新片榜</Text>
           </TabBarIOS.Item>
           <TabBarIOS.Item
@@ -62,8 +53,7 @@ export default class PageTabBar extends React.Component {
             icon={require('../../image/icon/tabbar/now-normal.png')}
             selectedIcon={require('../../image/icon/tabbar/now-active.png')}
             selected={this.state.activeTab === 'now'}
-            onPress={() => {this.toggleTab('now')}}
-          >
+            onPress={() => {this.toggleTab('now')}}>
             <Text>Search</Text>
           </TabBarIOS.Item>
           <TabBarIOS.Item
@@ -71,8 +61,7 @@ export default class PageTabBar extends React.Component {
             icon={require('../../image/icon/tabbar/coming-normal.png')}
             selectedIcon={require('../../image/icon/tabbar/coming-active.png')}
             selected={this.state.activeTab === 'coming'}
-            onPress={() => {this.toggleTab('coming')}}
-          >
+            onPress={() => {this.toggleTab('coming')}}>
             <Text>Search</Text>
           </TabBarIOS.Item>
           <TabBarIOS.Item
@@ -80,8 +69,7 @@ export default class PageTabBar extends React.Component {
             icon={require('../../image/icon/tabbar/search-normal.png')}
             selectedIcon={require('../../image/icon/tabbar/search-active.png')}
             selected={this.state.activeTab === 'search'}
-            onPress={() => {this.toggleTab('search')}}
-          >
+            onPress={() => {this.toggleTab('search')}}>
             <Text>Search</Text>
           </TabBarIOS.Item>
         </TabBarIOS>
---
+++ src/unit/index.js
@@ -0,0 +1,9 @@
+export default {
+  COLOR: {
+    YELLOW: '#fbb03d',
+    GREEN: '#23a06e',
+    BLUE: '#2490f8',
+    PURPLE: '#7f25fb',
+    TOP250: '#fc1368'
+  }
+}
