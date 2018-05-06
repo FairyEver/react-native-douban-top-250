24df91a4b90d76d8facc9aee70b68cd9e7abda09
liyang <1711467488@qq.com>
2018-05-06 22:12:21 +0800

    no message

--- src/page/AppIndex/index.js
+++ src/page/AppIndex/index.js
@@ -1,22 +1,39 @@
 import React from 'react';
 import { View, Text, TabBarIOS } from 'react-native';
 
-import PageTop250 from '../PageTop250'
+import PageTop250 from '../PageTop250';
 
 export default class PageTabBar extends React.Component {
 
   // 颜色设置
-  yellowOrange = '#fbb03d';
-  green = '#23a06e';
-  dodgerBlue = '#2490f8';
-  bluePurple = '#7f25fb';
+  COLOR_YELLOW = '#fbb03d';
+  COLOR_GREEN = '#23a06e';
+  COLOR_BLUE = '#2490f8';
+  COLOR_PURPLE = '#7f25fb';
   COLOR_TOP250 = '#fc1368';
 
   state = {
     activeTab: 'top',
     tintColor: this.COLOR_TOP250
   };
-  render() {
+
+  // 切换tab
+  toggleTab = (tabName) => {
+    let color = ''
+    switch (tabName) {
+      case 'top': color = this.COLOR_TOP250; break;
+      case 'new': color = this.COLOR_PURPLE; break;
+      case 'now': color = this.COLOR_YELLOW; break;
+      case 'coming': color = this.COLOR_BLUE; break;
+      default: color = this.COLOR_GREEN; break;
+    };
+    this.setState({
+      activeTab: tabName,
+      tintColor: color
+    })
+  };
+
+  render () {
     return (
       <View style={{flex: 1}}>
         <TabBarIOS
@@ -27,7 +44,7 @@ export default class PageTabBar extends React.Component {
             icon={require('../../image/icon/tabbar/fire-normal.png')}
             selectedIcon={require('../../image/icon/tabbar/fire-active.png')}
             selected={this.state.activeTab === 'top'}
-            onPress={() => {this.setState({activeTab: 'top'})}}
+            onPress={() => {this.toggleTab('top')}}
           >
             <PageTop250 navigator={this.props.navigator}/>
           </TabBarIOS.Item>
@@ -36,7 +53,7 @@ export default class PageTabBar extends React.Component {
             icon={require('../../image/icon/tabbar/new-normal.png')}
             selectedIcon={require('../../image/icon/tabbar/new-active.png')}
             selected={this.state.activeTab === 'new'}
-            onPress={() => {this.setState({activeTab: 'new'})}}
+            onPress={() => {this.toggleTab('new')}}
           >
             <Text>新片榜</Text>
           </TabBarIOS.Item>
@@ -45,7 +62,7 @@ export default class PageTabBar extends React.Component {
             icon={require('../../image/icon/tabbar/now-normal.png')}
             selectedIcon={require('../../image/icon/tabbar/now-active.png')}
             selected={this.state.activeTab === 'now'}
-            onPress={() => {this.setState({activeTab: 'now'})}}
+            onPress={() => {this.toggleTab('now')}}
           >
             <Text>Search</Text>
           </TabBarIOS.Item>
@@ -54,7 +71,7 @@ export default class PageTabBar extends React.Component {
             icon={require('../../image/icon/tabbar/coming-normal.png')}
             selectedIcon={require('../../image/icon/tabbar/coming-active.png')}
             selected={this.state.activeTab === 'coming'}
-            onPress={() => {this.setState({activeTab: 'coming'})}}
+            onPress={() => {this.toggleTab('coming')}}
           >
             <Text>Search</Text>
           </TabBarIOS.Item>
@@ -63,7 +80,7 @@ export default class PageTabBar extends React.Component {
             icon={require('../../image/icon/tabbar/search-normal.png')}
             selectedIcon={require('../../image/icon/tabbar/search-active.png')}
             selected={this.state.activeTab === 'search'}
-            onPress={() => {this.setState({activeTab: 'search'})}}
+            onPress={() => {this.toggleTab('search')}}
           >
             <Text>Search</Text>
           </TabBarIOS.Item>
@@ -71,4 +88,5 @@ export default class PageTabBar extends React.Component {
       </View>
     );
   }
+
 }
